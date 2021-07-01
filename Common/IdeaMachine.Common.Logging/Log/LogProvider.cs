b52f808e843def9346a0505b699e738c07813e25
Stefan Schranz <Sossenbinder@users.noreply.github.com>
2021-07-01 22:41:53 +0200

    - Added missing file

---
+++ Common/IdeaMachine.Common.Logging/Log/LogProvider.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Events;
+
+namespace IdeaMachine.Common.Logging.Log
+{
+	public static class LogProvider
+	{
+		public static ILogger CreateLogger(IConfiguration configuration)
+		{
+			var defaultLoggerConfiguration = new LoggerConfiguration()
+				.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+				.Enrich.FromLogContext()
+				.WriteTo.Console();
+
+			var seqServer = configuration["Seq_Server"];
+			if (seqServer != null)
+			{
+				defaultLoggerConfiguration = defaultLoggerConfiguration.WriteTo.Seq(seqServer);
+			}
+
+			var logger = Serilog.Log.Logger = defaultLoggerConfiguration.CreateLogger();
+
+			logger.Information("Logger initialized");
+
+			return logger;
+		}
+	}
+}
