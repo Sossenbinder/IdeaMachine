5bbe67cdd6ae8dbd525d85f2a4f2d56b367a7d50
Stefan Schranz <stefan.daniel.schranz96@gmail.com>
2021-09-29 22:54:27 +0200

    - More config stuff

--- Services/IdeaMachine.Service.Base/Utils/ConfigProvider/ProductionConfigProvider.cs
+++ Services/IdeaMachine.Service.Base/Utils/ConfigProvider/ProductionConfigProvider.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Azure.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace IdeaMachine.Service.Base.Utils.ConfigProvider
@@ -13,12 +14,20 @@ namespace IdeaMachine.Service.Base.Utils.ConfigProvider
 		protected override IConfigurationBuilder AddDeploymentConfig(IConfigurationBuilder configBuilder)
 		{
 			var intermediateConfig = configBuilder.Build();
-
+			
 			configBuilder = configBuilder.AddAzureKeyVault("https://ideamachine.vault.azure.net/", intermediateConfig["KeyVaultClientId"], intermediateConfig["KeyVaultClientSecret"]);
 
 			intermediateConfig = configBuilder.Build();
 
-			return configBuilder.AddAzureAppConfiguration(intermediateConfig["AppConfigConnectionString"]);
+			return configBuilder.AddAzureAppConfiguration(options =>
+			{
+				options.Connect(intermediateConfig["AppConfigConnectionString"])
+					.ConfigureKeyVault(kvConf =>
+					{
+						kvConf.SetCredential(new ClientSecretCredential("ff009f2c-d651-472e-b73e-e0f894c6011e",
+							intermediateConfig["KeyVaultClientId"], intermediateConfig["KeyVaultClientSecret"]));
+					});
+			});
 		}
 	}
 }
