c4595b946f14c6a7e0d52b8ea17c0d41a4818ed3
Stefan Schranz <stefan.daniel.schranz96@gmail.com>
2021-11-21 23:15:27 +0100

    - Added ServiceNameEnricher

---
+++ Common/IdeaMachine.Common.Logging/Log/ServiceNameEnricher.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Reflection;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace IdeaMachine.Common.Logging.Log
+{
+    public class ServiceNameEnricher : ILogEventEnricher
+    {
+	    private readonly string _serviceName;
+
+	    public ServiceNameEnricher()
+	    {
+		    var entryAssembly = Assembly.GetEntryAssembly();
+			
+		    _serviceName = entryAssembly?.GetName().Name ?? Dns.GetHostName();
+	    }
+
+	    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+	    {
+		    logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("ServiceName", _serviceName));
+	    }
+    }
+}
